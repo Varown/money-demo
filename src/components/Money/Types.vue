dc199437b713d31cac8d7d8fa5ced0766a81bdcb
PengMeng <383867421@qq.com>
2020-04-27 11:33:12 +0800

    基本完成NumberPad组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -19,46 +19,15 @@
   export default class Types extends Vue {
     type = '-';
     @Prop(Number) xxx: number | undefined;
-    //引用外部数据必须用@，否则会被认为是data
-    //Number是告诉vue xxx运行时它的数据类型
-    //number | undefined是告ts  xxx 编辑时的数据类型
-   //xxx 属性名
-
-
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
         throw new Error('type is unknown');
       }
       this.type = type;
     }
-    mounted(){
-      if(this.xxx===undefined){
-        console.log("类型不对")
-      }else {
-        console.log(this.xxx)
-      }
-  }
-
 
   }
 
-  /*export default {
-    name: 'Types',
-    data(){
-      return{
-        type:'-'//"-"号代表支出，“+”号代表收入
-      }
-    },
-    methods:{
-      selectType(type){
-        if(type!=='-'&&type!=='+'){
-        throw new Error('type is unknown')
-        }
-        this.type=type
-      }
-
-    }
-  };*/
 </script>
 
 <style lang="scss" scoped>
