36ca5a8345db161bbe3800a378933f4d0b2db83e
PengMeng <383867421@qq.com>
2020-04-29 15:35:09 +0800

    添加的标签可跳转至编辑页面

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -16,7 +16,6 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component, Prop} from 'vue-property-decorator';
-  import tagListModel from '@/models/tagListModel';
 
   @Component
   export default class Tags extends Vue {
