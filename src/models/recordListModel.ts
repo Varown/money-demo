a86d4c2d5b4121da0311fb68092501d53c4052df
PengMeng <383867421@qq.com>
2020-04-30 14:55:06 +0800

    封装了recordListModel.ts的操作到main.ts  （window） diff --git a/src/custom.d.ts b/src/custom.d.ts index 7dc08db..83d5c87 100644 --- a/src/custom.d.ts +++ b/src/custom.d.ts @@ -25,5 +25,6 @@ interface Window {    createTag: (name: string) => void;    removeTag: (id: string) => boolean;    updateTag: (id: string,name: string) => 'success' | 'not found' | 'duplicated'; - +  recordList: RecordItem[]; +  createRecord: (record:RecordItem)=>void;  } diff --git a/src/main.ts b/src/main.ts index ce54835..6ba5e72 100644 --- a/src/main.ts +++ b/src/main.ts @@ -7,13 +7,18 @@ import Nav from '@/components/Nav.vue';  import Layout from '@/components/Layout.vue';  import Icon from '@/components/Icon.vue';  import tagListModel from '@/models/tagListModel'; +import recordListModel from '@/models/recordListModel';
     Vue.config.productionTip = false;
    
     Vue.component('Nav', Nav);
     Vue.component('Layout', Layout);
     Vue.component('Icon', Icon);
    +//record store
    
    +window.recordList=recordListModel.fetch();
    +window.createRecord=(record: RecordItem)=>recordListModel.create(record)
    +//tag store
     window.tagList= tagListModel.fetch();
    
     window.findTag=(id: string)=>{
    diff --git a/src/models/recordListModel.ts b/src/models/recordListModel.ts
    index 645a5e4..f1c42c8 100644
    --- a/src/models/recordListModel.ts
    +++ b/src/models/recordListModel.ts
    @@ -8,7 +8,7 @@ create(record: RecordItem){
       const record2: RecordItem = clone(record)
       record2.createdAt = new Date();
       this.data.push(record2);
    -
    +  this.save()
     },
       fetch() {
         this.data = JSON.parse(window.localStorage.getItem('localStorageLKeyName') || '[]') as RecordItem[];
    diff --git a/src/views/Money.vue b/src/views/Money.vue
    index 6f39984..fceb62f 100644
    --- a/src/views/Money.vue
    +++ b/src/views/Money.vue
    @@ -19,11 +19,11 @@
       import Tags from '@/components/Money/Tags.vue';
    
       import {Component, Watch} from 'vue-property-decorator';
    -  import recordListModel from '@/models/recordListModel';
    +
    
       window.localStorage.setItem('version', '0.0.1');
    -  const recordList = recordListModel.fetch();
    +
    
       @Component({
    @@ -31,7 +31,7 @@
       })
       export default class Money extends Vue {
         tags = window.tagList;
    -    recordList: RecordItem[] = recordList;
    +    recordList= window.recordList;
         record: RecordItem = {
           tags: [], notes: '', type: '-', amount: 0
    
    @@ -53,13 +53,10 @@
         }
    
         saveRecord() {
    -      recordListModel.create(this.record)
    +      window.createRecord(this.record)
         }
    
    -    @Watch('recordList')
    -    onRecordListChange() {
    -      recordListModel.save();
    -    }
    +
       }
    
     </script>

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -8,7 +8,7 @@ create(record: RecordItem){
   const record2: RecordItem = clone(record)
   record2.createdAt = new Date();
   this.data.push(record2);
-
+  this.save()
 },
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem('localStorageLKeyName') || '[]') as RecordItem[];
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,11 +19,11 @@
   import Tags from '@/components/Money/Tags.vue';
 
   import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
+
 
 
   window.localStorage.setItem('version', '0.0.1');
-  const recordList = recordListModel.fetch();
+
 
 
   @Component({
@@ -31,7 +31,7 @@
   })
   export default class Money extends Vue {
     tags = window.tagList;
-    recordList: RecordItem[] = recordList;
+    recordList= window.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
 
@@ -53,13 +53,10 @@
     }
 
     saveRecord() {
-      recordListModel.create(this.record)
+      window.createRecord(this.record)
     }
 
-    @Watch('recordList')
-    onRecordListChange() {
-      recordListModel.save();
-    }
+
   }
 
 </script>
