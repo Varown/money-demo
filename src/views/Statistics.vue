71f90a8ec8cc2021853075f2dac8537960aa50ed
PengMeng <383867421@qq.com>
2020-05-02 16:52:37 +0800

    删除了Tapes.vue

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -22,7 +22,7 @@
 </style>
 
 <script lang="ts">
-  import Types from '@/components/Money/Types.vue';
+
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import Tabs from '@/components/Tabs.vue';
@@ -30,7 +30,7 @@
   import recordTypeList from '@/constans/recordTypeList';
 
   @Component({
-    components: {Tabs, Types},
+    components: {Tabs},
   })
   export default class Statistics extends Vue {
     type = '-';
